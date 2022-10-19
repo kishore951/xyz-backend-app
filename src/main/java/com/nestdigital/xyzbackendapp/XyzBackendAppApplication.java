004427fbf20b27b0cc1f743daf708d02e4fcb4df
kishore951 <kishoresakthivel951@gmail.com>
2022-10-19 08:51:54 +0530

    new

---
+++ src/main/java/com/nestdigital/xyzbackendapp/XyzBackendAppApplication.java
@@ -0,0 +1,13 @@
+package com.nestdigital.xyzbackendapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XyzBackendAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XyzBackendAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nestdigital/xyzbackendapp/XyzBackendAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nestdigital.xyzbackendapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XyzBackendAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
